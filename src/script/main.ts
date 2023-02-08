13d3bba048595d69beb6a98d44f38cfdba645ea9
sb <dev.simon.bengtsson@gmail.com>
2023-02-08 17:55:25 +0100

    removed interface and api

--- src/script/main.ts
+++ src/script/main.ts
@@ -1,57 +1,11 @@
-import '../style/style.css'
-import '../style/hero.css'
-import '../style/showcase.css'
-import '../style/searchbar.css'
-import '../style/footer.css'
-import '../style/menu.css'
-import '../style/breakpoints.css'
-
-/**
- * API
- */
-
-// Fetching movies from API
-export const fetchMovies = async () => {
-    const res = await fetch('https://imdb-top-100-movies.p.rapidapi.com/', options)
-
-    if (!res.ok) {
-        throw new Error(`Could not get movies, reason: ${res.status} ${res.statusText}`)
-    }
-    return await res.json()
-}
-
-export const options = {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application.json',
-        'X-RapidAPI-Key': '7b1dc438damsh4b7942ffbd9b89ap1a8c0ajsn3730c02dab28',
-        'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com',
-        'X-Content-Type': 'Options: nosniff'
-
-    },
-    
-}
-
-/**
- * Interface
- */
-
-export interface IMovie {
-    description: string,
-    director: Array<string>,
-    genre: Array<string>,
-    id: string,
-    image: string,
-    imdbid: string,
-    rank: number,
-    rating: number,
-    thumbnail: string,
-    title: string,
-    trailer: string,
-    writers: Array<string>,
-    year: number
-}
-
+import '@style/style.css'
+import '@style/hero.css'
+import '@style/showcase.css'
+import '@style/searchbar.css'
+import '@style/footer.css'
+import '@style/menu.css'
+import '@style/breakpoints.css'
+import { fetchMovies } from './api'
 
 
 let movies: IMovie[] = []
