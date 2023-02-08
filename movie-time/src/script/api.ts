c6bee6aac08c376474fcc6e95bd838984f241cf4
sb <dev.simon.bengtsson@gmail.com>
2023-02-08 14:32:54 +0100

    added api and interface files for better structure

---
+++ movie-time/src/script/api.ts
@@ -0,0 +1,24 @@
+/**
+ * API
+ */
+
+
+// Fetching movies from API
+export const fetchMovies = async () => {
+    const res = await fetch('https://imdb-top-100-movies.p.rapidapi.com/', options)
+
+    if (!res.ok) {
+        throw new Error(`Could not get movies, reason: ${res.status} ${res.statusText}`)
+    }
+    return await res.json()
+}
+
+export const options = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application.json',
+        'X-RapidAPI-Key': '7b1dc438damsh4b7942ffbd9b89ap1a8c0ajsn3730c02dab28',
+        'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
+    }
+}
+
