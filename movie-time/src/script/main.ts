4dc4d473e987199fa6973314fd2994694cd69f49
sb <dev.simon.bengtsson@gmail.com>
2023-02-08 16:10:23 +0100

    moved interface and api into main due to netlify bugs

--- movie-time/src/script/main.ts
+++ movie-time/src/script/main.ts
@@ -5,8 +5,54 @@ import '../style/searchbar.css'
 import '../style/footer.css'
 import '../style/menu.css'
 import '../style/breakpoints.css'
-import {fetchMovies} from '../script/api'
-import {IMovie} from '../script/interface'
+
+/**
+ * API
+ */
+
+// Fetching movies from API
+export const fetchMovies = async () => {
+    const res = await fetch('https://imdb-top-100-movies.p.rapidapi.com/', options)
+
+    if (!res.ok) {
+        throw new Error(`Could not get movies, reason: ${res.status} ${res.statusText}`)
+    }
+    return await res.json()
+}
+
+export const options = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application.json',
+        'X-RapidAPI-Key': '7b1dc438damsh4b7942ffbd9b89ap1a8c0ajsn3730c02dab28',
+        'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com',
+        'X-Content-Type': 'Options: nosniff'
+
+    },
+    
+}
+
+/**
+ * Interface
+ */
+
+export interface IMovie {
+    description: string,
+    director: Array<string>,
+    genre: Array<string>,
+    id: string,
+    image: string,
+    imdbid: string,
+    rank: number,
+    rating: number,
+    thumbnail: string,
+    title: string,
+    trailer: string,
+    writers: Array<string>,
+    year: number
+}
+
+
 
 let movies: IMovie[] = []
 let topTen: IMovie[] = []
