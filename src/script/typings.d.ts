dc0a5ea58eeca1a93ab3eddbeb2a3043c58ac7e8
sb <dev.simon.bengtsson@gmail.com>
2023-02-08 17:55:15 +0100

    added interface to typings

---
+++ src/script/typings.d.ts
@@ -0,0 +1,15 @@
+interface IMovie {
+	description: string
+	director: Array<string>
+	genre: Array<string>
+	id: string
+	image: string
+	imdbid: string
+	rank: number
+	rating: number
+	thumbnail: string
+	title: string
+	trailer: string
+	writers: Array<string>
+	year: number
+}
