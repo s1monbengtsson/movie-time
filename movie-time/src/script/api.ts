81819c8398c9734bcc02e414d8a8ec838c1df4fc
sb <dev.simon.bengtsson@gmail.com>
2023-02-08 15:48:17 +0100

    added nosniff

--- movie-time/src/script/api.ts
+++ movie-time/src/script/api.ts
@@ -18,7 +18,10 @@ export const options = {
     headers: {
         'Content-Type': 'application.json',
         'X-RapidAPI-Key': '7b1dc438damsh4b7942ffbd9b89ap1a8c0ajsn3730c02dab28',
-        'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
-    }
+        'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com',
+        'X-Content-Type': 'Options: nosniff'
+
+    },
+    
 }
 
